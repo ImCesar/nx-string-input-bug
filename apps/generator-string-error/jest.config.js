e9f0878387377075f45e6bf4d6a4175f1a4205ba
Cesar Avitia <cesar.avitia@carvana.com>
2021-05-13 10:55:37 -0700

    initial commit

---
+++ apps/generator-string-error/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'generator-string-error',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/generator-string-error',
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/generator-string-error'],
+};
