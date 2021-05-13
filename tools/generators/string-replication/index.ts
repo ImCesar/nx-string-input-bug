8113d72b947b5abd09e13901101fdfaa85d1eda7
Cesar Avitia <cesar.avitia@carvana.com>
2021-05-13 13:41:45 -0700

    uadd generator that fails

---
+++ tools/generators/string-replication/index.ts
@@ -0,0 +1,6 @@
+import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
+
+export default async function (host: Tree, schema: any) {
+  console.log(schema.name)
+}
